65fd18ce289dffdf85a291b3ddf106b5ffbfe1ff
thalles0112 <athalles80@gmail.com>
2024-11-18 09:39:23 -0300

    changed sqlite to mysql

--- database/db.js
+++ database/db.js
@@ -1,21 +1,28 @@
 import { Sequelize } from "sequelize";
 
 const database = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+    dialect: 'mysql',
+    database: 'sql10745504',
+    user: 'sql10745504',
+    password: 'vMyHxjftqS',
+    host: 'sql10.freesqldatabase.com',
+    port: 3306,
+    username: 'sql10745504'
+    
 })
 
 
 try{
     database.authenticate()
+    database.sync()
 }
 
 catch (err){
     console.error('nao deu pra entrar no banco de dados por que:',err)
 }
 finally{
-    console.log('deu sim')
-    database.sync()
+    console.log('banco de dados autenticado')
+    
 }
 
 
